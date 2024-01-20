f80fce4ce22e3306105229dde4a4723149fe3e12
akarshbr <akarshbrajeev@gmail.com>
2024-01-20 18:56:27 +0530

    Splash screen added

---
+++ lib/view/main.dart
@@ -0,0 +1,6 @@
+import 'package:flick/view/splash.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Splash()));
+}
---
+++ lib/view/splash.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 4), () {});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: const Image(image: AssetImage("assets/logo/flickblack.png"), height: 50),
+      ),
+    );
+  }
+}
