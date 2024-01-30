35097768fb373af53975863592f95ade6fb7c92e
akarshbr <akarshbrajeev@gmail.com>
2024-01-30 19:48:42 +0530

    current screen added

---
+++ lib/view/current_Screen/current_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flick/view/custom_widget/app_bar.dart';
+import 'package:flick/view/custom_widget/bottom_navigaton.dart';
+import '../../utils/constants.dart';
+import '../bottom_navigation_screens/movie/movie_screen.dart';
+import '../bottom_navigation_screens/series/series_screen.dart';
+import '../bottom_navigation_screens/settings/settings_screen.dart';
+import '../bottom_navigation_screens/watchlist/watchlist_screen.dart';
+//import 'package:flick/view/bottom_navigation_screens/discussion/discussion_screen.dart';
+
+class CurrentScreen extends StatelessWidget {
+  CurrentScreen({super.key});
+
+  final _screens = [
+    const MovieScreen(),
+    const SeriesScreen(),
+    const WatchListScreen(),
+    const SettingsScreen(),
+    //const DiscussionScreen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: const PreferredSize(preferredSize: appBarPreferredSize, child: AppBarWidget()),
+      body: ValueListenableBuilder(
+          valueListenable: indexChangeNotifier,
+          builder: (context, index, _) {
+            return _screens[index];
+          }),
+      bottomNavigationBar: const BottomNavigationWidget(),
+    );
+  }
+}
