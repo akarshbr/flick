a22126d095c5527741b05011617ed51a4e1761c2
akarshbr <akarshbrajeev@gmail.com>
2024-03-24 14:52:18 +0530

    config class and api helper class added

---
+++ lib/config/config.dart
@@ -0,0 +1,4 @@
+class AppConfig{
+  static String baseURL = "https://api.themoviedb.org/3/";
+  static String apiKey = "6d2b9681525d9d62b47c2f7445f894c2";
+}
---
+++ lib/repository/helper/api_helper.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flick/config/config.dart';
+import 'package:http/http.dart' as http;
+
+class ApiHelper {
+  static getData({required String endpoint}) async {
+    log("Api Helper class -> getData");
+    final url = Uri.parse(AppConfig.baseURL + endpoint);
+    try {
+      var response = await http.get(url);
+      log("API called -> status code : ${response.statusCode}");
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        log(decodedData.toString());
+        var data = {"status": 1, "data": decodedData};
+        return data;
+      } else {
+        log("API failed");
+        var data = {"status": 0, "data": null};
+        return data;
+      }
+    } catch (e) {
+      log("$e");
+    }
+  }
+}
